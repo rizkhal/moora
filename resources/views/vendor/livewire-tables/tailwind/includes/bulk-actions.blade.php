2b49c9679716488575801fd626925afa8e4a3817
rizkhal <lamaaurizkhal@gmail.com>
2021-06-23 22:32:27 +0800

    wire:click.prevent

--- resources/views/vendor/livewire-tables/tailwind/includes/bulk-actions.blade.php
+++ resources/views/vendor/livewire-tables/tailwind/includes/bulk-actions.blade.php
@@ -41,7 +41,7 @@
                     <div class="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
                         @foreach($bulkActions as $action => $title)
                             <button
-                                wire:click="{{ $action }}"
+                                wire:click.prevent="{{ $action }}"
                                 wire:key="bulk-action-{{ $action }}"
                                 type="button"
                                 class="block w-full px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 dark:bg-gray-700 dark:text-gray-200 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900 flex items-center space-x-2"
